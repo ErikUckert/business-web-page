c95e87ff7734a9daff8e0d7b1f49e9a85ef6455a
Erik Uckert <uckert@outlook.com>
2021-02-18 10:24:50 +0100

    add prototype for hiding div - start with contact section - add simple button and behavior to expand and collapse contact section

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function myFunction() {
       x.className = x.className.replace(" w3-show", "");
     }
   }
+
+  function hideDiv() {
+    var x = document.getElementById("hide");
+    if (x.style.display === "none") {
+        console.log(x.style.display)
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
