108245d7391df5c0251bccc2f85464b8a5ca5e3c
Erik Uckert <uckert@outlook.com>
2021-02-18 12:17:42 +0100

    add more buttons to hide imprint and data

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
+function hideNav() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
       x.className += " w3-show";
@@ -8,10 +8,18 @@ function myFunction() {
     }
   }
 
-  function hideDiv() {
-    var x = document.getElementById("hide");
+  function hideContact() {
+    var x = document.getElementById("hideContact");
     if (x.style.display === "none") {
-        console.log(x.style.display)
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hideImprint() {
+    var x = document.getElementById("hideImprint");
+    if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
