1b0561ce32822975c0f028f180cd5e79c894a3e5
Erik Uckert <erik@Eriks-MacBook-Air.local>
2021-03-11 23:40:07 +0100

    do some prettifying with in intendation

--- script.js
+++ script.js
@@ -7,52 +7,52 @@ const divData = document.getElementById("data");
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function hideNav() {
-    if (divNav.className.indexOf("w3-show") == -1) {
-      divNav.className += " w3-show";
-    } else { 
-      divNav.className = divNav.className.replace(" w3-show", "");
-    }
-  }
+	if (divNav.className.indexOf("w3-show") == -1) {
+		divNav.className += " w3-show";
+	} else {
+		divNav.className = divNav.className.replace(" w3-show", "");
+	}
+}
 
-  // functions for toggle info sections
-  function hideContact() {
-    if (divContact.style.display === "none") {
-      divContact.style.display = "block";
-      divImprint.style.display = "none";
-      divData.style.display = "none";
-    } else {
-      divContact.style.display = "none";
-    }
-  }
+// functions for toggle info sections
+function hideContact() {
+	if (divContact.style.display === "none") {
+		divContact.style.display = "block";
+		divImprint.style.display = "none";
+		divData.style.display = "none";
+	} else {
+		divContact.style.display = "none";
+	}
+}
 
-  function hideImprint() {
-    if (divImprint.style.display === "none") {
-      divImprint.style.display = "block";
-      divContact.style.display = "none";
-      divData.style.display = "none";
-    } else {
-      divImprint.style.display = "none";
-    }
-  }
+function hideImprint() {
+	if (divImprint.style.display === "none") {
+		divImprint.style.display = "block";
+		divContact.style.display = "none";
+		divData.style.display = "none";
+	} else {
+		divImprint.style.display = "none";
+	}
+}
 
-  function hideData() {
-    if (divData.style.display === "none") {
-      divData.style.display = "block";
-      divImprint.style.display = "none";
-      divContact.style.display = "none";
-    } else {
-      divData.style.display = "none";
-    }
-  }
+function hideData() {
+	if (divData.style.display === "none") {
+		divData.style.display = "block";
+		divImprint.style.display = "none";
+		divContact.style.display = "none";
+	} else {
+		divData.style.display = "none";
+	}
+}
 
-  /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    divNavBar.style.top = "0";
-  } else {
-    divNavBar.style.top = "-300px";
-  }
-  prevScrollpos = currentScrollPos;
+	var currentScrollPos = window.pageYOffset;
+	if (prevScrollpos > currentScrollPos) {
+		divNavBar.style.top = "0";
+	} else {
+		divNavBar.style.top = "-300px";
+	}
+	prevScrollpos = currentScrollPos;
 }
